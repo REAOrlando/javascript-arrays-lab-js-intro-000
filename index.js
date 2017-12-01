ee542886aea355b6d9e4f93db4ed82c4b43121b8
Chris Albanese <chris@reaorlando.com>
2017-12-01 04:20:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var newArray = [...kittens];
+  var newArray = kittens;
   newArray.push(name);
   return newArray;
 }
